2714e22b9944d00d5380213cd48826c6fd0f7fe1
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-12 21:10:32 +0300

    Create web_flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return 'Python %s' % (text.replace('_', ' ') or 'is cool')
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def python(n):
+    if n is int:
+        return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
