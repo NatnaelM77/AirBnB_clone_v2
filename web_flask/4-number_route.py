9453bb0d129f92a6859cd15a342788d7f5f94ace
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-12 22:30:51 +0300

    Quick Fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,7 +32,7 @@ def python(text='is_cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return '%d is number' % n
+    return '%d is a number' % n
 
 
 if __name__ == '__main__':
